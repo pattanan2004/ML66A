6a442a5f4c91f3850b7edaede0074625830bab81
Chaliptxn <1166105050320@mail.rmutt.ac.th>
2026-02-25 16:25:39 +0700

    Update multiple_classification.py

--- multiple_classification.py
+++ multiple_classification.py
@@ -11,11 +11,12 @@ from streamlit_option_menu import option_menu
 
 riding_model = pickle.load(open("Riding_model.sav",'rb'))
 loan_model = pickle.load(open("loan_model.sav",'rb'))
+bmi_model = pickle.load(open("bmi_model.sav",'rb'))
 
 
 with st.sidebar:
     selected = option_menu(
-        'Classification',['Loan','Riding']
+        'Classification',['Loan','Riding','bmi']
         )
 
 gender_map = {
@@ -127,4 +128,5 @@ if(selected == 'Riding'):
           
 
     st.success(Riding_prediction)
+
 
